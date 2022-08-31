54896a4f1f08de820b8ff5bda884ee52be86d2ec
Azlan <mhdazlanabaziz@gmail.com>
2022-08-31 21:07:04 +0800

    second commit

--- cariWang/Form1.Designer.vb
+++ cariWang/Form1.Designer.vb
@@ -27,7 +27,7 @@ Partial Class Form1
         '
         'Button1
         '
-        Me.Button1.Location = New System.Drawing.Point(50, 52)
+        Me.Button1.Location = New System.Drawing.Point(43, 87)
         Me.Button1.Name = "Button1"
         Me.Button1.Size = New System.Drawing.Size(75, 23)
         Me.Button1.TabIndex = 0
